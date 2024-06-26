5d6fa0ca8c2b3159459feeff5372a209a955af69
ysmltr <yesimltr1@gmail.com>
2024-06-26 17:43:24 +0300

    feat: Update app component to include HomeComponent
    This commit updates the app component to include the HomeComponent. The HomeComponent is imported and added to the template of the app component. This change was made to enhance the functionality of the app and improve the user experience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HomeComponent],  // Burada AppHomeComponent'i import edin
+  template: `
+    <p>Merhaba</p>
+    <hr>
+    <div style="color: #691FB7;">
+      <app-home></app-home>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'yesim ilter';
